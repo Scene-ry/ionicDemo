6ec4dacc3c5c6195e857a04f43ed17b09aa55817
Scene-ry <cy3wjx@126.com>
2016-12-08 17:41:08 +0800

    avatar test

--- www/js/controller/index-ctrl.js
+++ www/js/controller/index-ctrl.js
@@ -1,185 +1,239 @@
-app.controller('indexCtrl', function($scope, $http, $cordovaCamera, $cordovaImagePicker, $ionicActionSheet, $ionicPopup, $ionicSlideBoxDelegate, $ionicScrollDelegate) {
+app.controller('indexCtrl',
+  function($scope,
+           $http,
+           $cordovaCamera,
+           $cordovaFile,
+           $cordovaImagePicker,
+           $ionicActionSheet,
+           $ionicPopup,
+           $ionicSlideBoxDelegate,
+           $ionicScrollDelegate) {
 
-  $scope.mainPageSlider = 0;
-  $scope.isImageShow = true;
+    $scope.mainPageSlider = 0;
 
-  var tabItems = $('.tab-item');
+    // bottom tab items
+    var tabItems = $('.tab-item');
 
-  // show error message
-  var showErrorMsg = function(msg) {
-    $ionicPopup.alert({
-      title: '错误',
-      template: msg
-    });
-  }
+    // app settings
+    $scope.settings = window.localStorage['idemo-settings'] ? JSON.parse(window.localStorage['idemo-settings']) : {
+      avatarSrc: 'img/ionic.png',
+      isImageShow: true,
+      rssUrl: 'http://feed.cnblogs.com/news/rss',
+      collections: []
+    }
+    var saveSettings = function() {
+      window.localStorage['idemo-settings'] = JSON.stringify($scope.settings);
+    }
 
-  // remove loading panel when finished
-  var finishLoading = function() {
-    $('ion-pane').css({ 'visibility': 'visible' });
-    $('.idemo-loading-cover').removeClass('visible');
-  }
-
-  // get rss test
-  $scope.rssUrl = window.localStorage['RssUrl'] || 'http://feed.cnblogs.com/news/rss';
-  $scope.refreshRssList = function() {
-    // $.ajax({
-    //   type: 'GET',
-    //   url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent($scope.rssUrl),
-    //   dataType: 'json',
-    //   error: function() {
-    //     alert("feed read failed.");
-    //   },
-    //   success: function(response) {
-    //     //console.log(response.responseData.feed.entries);
-    //     //$setTimeout(function() {}, 100000);
-    //     $scope.rssList = response.responseData.feed.entries;
-    //     $scope.$apply();
-    //   },
-    //   complete: function() {
-    //     $scope.$broadcast('scroll.refreshComplete');
-    //   }
-    // });
-
-    $http.jsonp('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%20%3D%20\'' + encodeURIComponent($scope.rssUrl) + '\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=JSON_CALLBACK')
-      .success(function(response) {
-        $scope.rssList = response.query.results.rss.channel.item;
-      }).error(function() {
-        alert("feed read failed.");
-      }).finally(function() {
-        $scope.$broadcast('scroll.refreshComplete');
-        finishLoading();
+    // show error message
+    var showErrorMsg = function(errLoc, msg) {
+      $ionicPopup.alert({
+        title: '错误',
+        template: errLoc + "<br>" + (msg.message ? msg.message : JSON.stringify(msg)) 
       });
+    }
 
-  };
+    // remove loading panel when finished
+    var finishLoading = function() {
+      $('ion-pane').css({ 'visibility': 'visible' });
+      $('.idemo-loading-cover').removeClass('visible');
+    }
 
-  // change color of tab icon when slider slides
-  $scope.slideHasChanged = function(selectedIndex) {
-    tabItems.each(function() {
-      $(this).removeClass('idemo-selected');
-    });
-    tabItems.eq(selectedIndex).addClass('idemo-selected');
-  };
+    // get rss test
+    $scope.refreshRssList = function() {
+      // $.ajax({
+      //   type: 'GET',
+      //   url: 'https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent($scope.rssUrl),
+      //   dataType: 'json',
+      //   error: function() {
+      //     alert("feed read failed.");
+      //   },
+      //   success: function(response) {
+      //     $scope.rssList = response.responseData.feed.entries;
+      //     $scope.$apply();
+      //   },
+      //   complete: function() {
+      //     $scope.$broadcast('scroll.refreshComplete');
+      //   }
+      // });
+      $http.jsonp('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=JSON_CALLBACK&q=' + encodeURIComponent($scope.settings.rssUrl))
+        .success(function(response) {
+          $scope.rssList = response.responseData.feed.entries;
+        }).error(function() {
+          alert("feed read failed.");
+        }).finally(function() {
+          $scope.$broadcast('scroll.refreshComplete');
+          finishLoading();
+        });
 
 
-  // show switch rss popup
-  $scope.showSwitchRssPrompt = function() {
-    $ionicPopup.show({
-      template: '<input type="text" ng-model="rssUrl" placeholder="Enter address...">',
-      title: '输入RSS源网址:',
-      scope: $scope,
-      buttons: [
-        { text: '取消' },
-        {
-          text: '<b>保存</b>',
-          type: 'button-positive',
-          onTap: function(e) {
-            if (!$scope.rssUrl) {
-              e.preventDefault();
-            } else {
-              return $scope.rssUrl;
+      // $http.jsonp('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%20%3D%20\'' + encodeURIComponent($scope.rssUrl) + '\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=JSON_CALLBACK')
+      //   .success(function(response) {
+      //     $scope.rssList = response.query.results.rss.channel.item;
+      //   }).error(function() {
+      //     alert("feed read failed.");
+      //   }).finally(function() {
+      //     $scope.$broadcast('scroll.refreshComplete');
+      //     finishLoading();
+      //   });
+
+      // $http.jsonp('http://10x.222x.46x.15x/?url=' + $scope.settings.rssUrl + "&callback=JSON_CALLBACK")
+      //   .success(function(response) {
+      //     $scope.rssList = response.data;
+      //   }).error(function() {
+      //     alert("feed read failed.");
+      //   }).finally(function() {
+      //     $scope.$broadcast('scroll.refreshComplete');
+      //     finishLoading();
+      //   });
+
+    };
+
+    // change color of tab icon when slider slides
+    $scope.slideHasChanged = function(selectedIndex) {
+      tabItems.each(function() {
+        $(this).removeClass('idemo-selected');
+      });
+      tabItems.eq(selectedIndex).addClass('idemo-selected');
+    };
+
+    // show switch rss popup
+    $scope.showSwitchRssPrompt = function() {
+      $ionicPopup.show({
+        template: '<input type="text" ng-model="settings.rssUrl" placeholder="Enter address...">',
+        title: '输入RSS源网址:',
+        scope: $scope,
+        buttons: [
+          { text: '取消' },
+          {
+            text: '<b>保存</b>',
+            type: 'button-positive',
+            onTap: function(e) {
+              if (!$scope.settings.rssUrl) {
+                e.preventDefault();
+              } else {
+                return $scope.settings.rssUrl;
+              }
             }
           }
+        ]
+      }).then(function(res) {
+        if (res) {
+          saveSettings();
+          $scope.refreshRssList();
         }
-      ]
-    }).then(function(res) {
-      if (res) {
-        window.localStorage['RssUrl'] = res;
-        $scope.refreshRssList();
-      }
-    });
-  };
-
-
-  // open picture library
-  var openPictureLib = function() {
-    // TODO
-    var options = {
-      maximumImagesCount: 10,
-      width: 800,
-      height: 800,
-      quality: 80
-    };
-
-    $cordovaImagePicker.getPictures(options)
-      .then(function (results) {
-        for (var i = 0; i < results.length; i++) {
-          console.log('Image URI: ' + results[i]);
-        }
-      }, function(error) {
-        // error getting photos
       });
-    console.log("pic library opened");
-  };
-
-  // open camera to change user avatar
-  var openCamera = function() {
-    // TODO
-    var options = {
-      destinationType: Camera.DestinationType.FILE_URI,
-      sourceType: Camera.PictureSourceType.CAMERA,
     };
 
-    $cordovaCamera.getPicture(options).then(function(imageURI) {
-      var image = document.getElementById('idemo-user-avatar');
-      image.src = imageURI;
-    }, function(err) {
-      // error
-    });
+    // copy image to cordova directory
+    var copyImageAndSetAvatar = function(imageURI) {
+      console.log("Image URI:" + imageURI);
 
+      var lastSlashIndex = imageURI.lastIndexOf('/');
+      var dirName = imageURI.substr(0, lastSlashIndex);
+      var fileName = imageURI.substr(lastSlashIndex + 1);
 
-    $cordovaCamera.cleanup(); // only for FILE_URI
+      // var check = $cordovaFile.checkFile(cordova.file.dataDirectory, 'avatarrrr.jpg');
+      // console.log("Check:" + JSON.stringify(check));
 
-    console.log("camera opened");
-  };
+      $cordovaFile.copyFile(dirName, fileName, cordova.file.dataDirectory, 'avatar.jpg')
+        .then(function(success) {
+          console.log(JSON.stringify(success));
+          $scope.settings.avatarSrc = success.nativeURL;
+          saveSettings();
+        }, function(error) {
+          showErrorMsg("copyImageAndSetAvatar", error);
+        });
 
+    };
 
-  // popup to choose local pic or take photo
-  $scope.showChangeAvatarPopup = function() {
-    $ionicActionSheet.show({
-      buttons: [
-        { text: '<i class="icon ion-image"></i>本地图片' },
-        { text: '<i class="icon ion-camera"></i>拍照上传' }
-      ],
-      titleText: '修改头像',
-      buttonClicked: function(index) {
-        try {
-          switch (index) {
-            case 0:
-              openPictureLib();
-              break;
-            case 1:
-              openCamera();
-              break;
-            default:
-              break;
+    // open picture library
+    var openPictureLib = function() {
+      var options = {
+        maximumImagesCount: 1,
+        width: 50,
+        height: 50,
+        quality: 80
+      };
+
+      $cordovaImagePicker.getPictures(options)
+        .then(function (results) {
+          if (results.length > 0) {
+            copyImageAndSetAvatar(results[0]);
           }
-        } catch(e) {
-          showErrorMsg(e);
+        }, function(error) {
+          // error getting photos
+          showErrorMsg("openPictureLib", error);
+        });
+    };
+
+    // open camera to change user avatar
+    var openCamera = function() {
+      var options = {
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA,
+        allowEdit: false,
+        encodingType: Camera.EncodingType.JPEG
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageURI) {
+        //console.log(imageURI);
+        copyImageAndSetAvatar(imageURI);
+      }, function(error) {
+        // error
+        //console.log(error);
+        showErrorMsg("openCamera", error);
+      });
+
+      //$cordovaCamera.cleanup(); // only for FILE_URI
+    };
+
+    // popup to choose local pic or take photo
+    $scope.showChangeAvatarPopup = function() {
+      $ionicActionSheet.show({
+        buttons: [
+          { text: '<i class="icon ion-image"></i>本地图片' },
+          { text: '<i class="icon ion-camera"></i>拍照上传' }
+        ],
+        titleText: '修改头像',
+        buttonClicked: function(index) {
+          try {
+            switch (index) {
+              case 0:
+                openPictureLib();
+                break;
+              case 1:
+                openCamera();
+                break;
+              default:
+                break;
+            }
+          } catch(e) {
+            showErrorMsg("showChangeAvatarPopup", e);
+          }
+          return true;
         }
-        return true;
-      }
+      });
+    };
+
+    // show image or not
+    $scope.isImageShowChange = function() {
+      saveSettings();
+    };
+
+    // switch slider when tab icon clicked
+    tabItems.each(function(index) {
+      $(this).click(function() {
+        if (index == 0 && $scope.mainPageSlider == 0) {
+          $ionicScrollDelegate.scrollTop();
+          return;
+        }
+        $ionicSlideBoxDelegate.slide(index);
+      });
     });
-  };
 
+    // initialize list
+    $scope.refreshRssList();
 
-  // show image or not
-  $scope.isImageShowChange = function() {
-    $scope.isImageShow = !$scope.isImageShow;
-  };
-
-
-  // switch slider when tab icon clicked
-  tabItems.each(function(index) {
-    $(this).click(function() {
-      if (index == 0 && $scope.mainPageSlider == 0) {
-        $ionicScrollDelegate.scrollTop();
-        return;
-      }
-      $ionicSlideBoxDelegate.slide(index);
-    });
-  });
-
-  // initialize list
-  $scope.refreshRssList();
-
-});
+  }
+);
--- www/js/filter/img-filter.js
+++ www/js/filter/img-filter.js
@@ -1,5 +1,5 @@
 app.filter('imgFilter', function() {
   return function(input) {
-  	return input.replace(/<img/g, '<img ng-if="isImageShow"').replace(/src="\/\/images/g, 'src="http://images');
+  	return input.replace(/<img/g, '<img ng-if="settings.isImageShow"').replace(/src="\/\//g, 'src="http://');
   };
 });
